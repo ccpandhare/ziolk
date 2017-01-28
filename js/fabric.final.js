72c35258bbd264b9af1e94d5637ed71fab04aaa1
Chinmay Pandhare <pandharechinmay882@gmail.com>
2017-01-28 15:07:33 +0530

    lite

--- js/fabric.final.js
+++ js/fabric.final.js
@@ -132,6 +132,34 @@ document.activecanvas = canvasfront;
     if(object.get(type) == "normal" || object.get(type) == "none") element.removeClass('active');
     else element.addClass('active');
   }
+  //Scale canvas
+  function scaleCanvas(canvas,SCALE_FACTOR) {
+    var canvasScale = 1;
+    canvasScale = canvasScale * SCALE_FACTOR;
+    var canvas = document.activecanvas;
+    canvas.setHeight(canvas.getHeight() * SCALE_FACTOR);
+    canvas.setWidth(canvas.getWidth() * SCALE_FACTOR);
+    canvas.discardActiveObject();
+    var objects = canvas.getObjects();
+    for (var i in objects) {
+        var scaleX = objects[i].scaleX;
+        var scaleY = objects[i].scaleY;
+        var left = objects[i].left;
+        var top = objects[i].top;
+
+        var tempScaleX = scaleX * SCALE_FACTOR;
+        var tempScaleY = scaleY * SCALE_FACTOR;
+        var tempLeft = left * SCALE_FACTOR;
+        var tempTop = top * SCALE_FACTOR;
+
+        objects[i].scaleX = tempScaleX;
+        objects[i].scaleY = tempScaleY;
+        objects[i].left = tempLeft;
+        objects[i].top = tempTop;
+
+        objects[i].setCoords();
+    }
+  }
 
   //render image from select
   function renderImage(file) {
--- js/master.js
+++ js/master.js
@@ -33,35 +33,15 @@ $(".color").spectrum({
     allowEmpty: true
 });
 $('.exportfront').click(function(){
-  var canvasScale = 1;
-  var SCALE_FACTOR = 2;
-  canvasScale = canvasScale * SCALE_FACTOR;
-  var canvas = document.activecanvas;
-  canvas.setHeight(canvas.getHeight() * SCALE_FACTOR);
-  canvas.setWidth(canvas.getWidth() * SCALE_FACTOR);
-  canvas.discardActiveObject();
-  var objects = canvas.getObjects();
-  for (var i in objects) {
-      var scaleX = objects[i].scaleX;
-      var scaleY = objects[i].scaleY;
-      var left = objects[i].left;
-      var top = objects[i].top;
-
-      var tempScaleX = scaleX * SCALE_FACTOR;
-      var tempScaleY = scaleY * SCALE_FACTOR;
-      var tempLeft = left * SCALE_FACTOR;
-      var tempTop = top * SCALE_FACTOR;
-
-      objects[i].scaleX = tempScaleX;
-      objects[i].scaleY = tempScaleY;
-      objects[i].left = tempLeft;
-      objects[i].top = tempTop;
-
-      objects[i].setCoords();
+  canvasses = [canvasfront, canvasback, canvasleft, canvasright];
+  var img = "";
+  for (var i in canvasses) {
+    scaleCanvas(canvasses[i],1.2);
+    var data = document.getElementById("canvasfront").toDataURL("image/png");
+    img +=
   }
-
-  canvas.renderAll();
+  canvasfront.renderAll();
   var img = document.getElementById("canvasfront").toDataURL("image/png");
   //console.log(img);
-  document.write("<img src=\""+img+"\">");
+  //document.write("<img src=\""+img+"\">");
 });
